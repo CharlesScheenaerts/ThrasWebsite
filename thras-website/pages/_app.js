d8d12ce2da3dba3dcec013206c5a44f0d2fc73fb
Charles Scheenaerts <charles.scheenaerts@gmail.com>
2025-10-18 23:56:33 +0200

    first test on iphone

--- thras-website/pages/_app.js
+++ thras-website/pages/_app.js
@@ -1,11 +1,14 @@
-import '../styles/globals.css';
-import Layout from '../components/Layout';
+// Dans pages/_app.js
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Component {...pageProps} />
-    </Layout>
+    </>
   );
 }
 
