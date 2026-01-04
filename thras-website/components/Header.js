1f8a8acd9f93770c642a86ae9da552b025260f82
Charles Scheenaerts <charles.scheenaerts@gmail.com>
2026-01-04 19:44:35 +0100

    Creation of the burger and fix of about thras upper margin

--- thras-website/components/Header.js
+++ thras-website/components/Header.js
@@ -8,45 +8,81 @@ export function Header() {
     const [scrollPercentage, setScrollPercentage] = useState(0);
     const [navbarScrolled, setNavbarScrolled] = useState(false);
     const [scrollUpVisible, setScrollUpVisible] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
 
     useEffect(() => {
         externalScrollUpVisible = setScrollUpVisible;
         const handleScroll = () => {
-        const scrollTop = window.scrollY;
-        const docHeight = document.body.scrollHeight - window.innerHeight;
-        const scrollPercent = scrollTop / docHeight;
-        setScrollPercentage(scrollPercent > 1 ? 1 : scrollPercent < 0 ? 0 : scrollPercent);
-        
-        setNavbarScrolled(scrollTop > 50);
-        setScrollUpVisible(scrollTop > 500);
+            const scrollTop = window.scrollY;
+            const docHeight = document.body.scrollHeight - window.innerHeight;
+            const scrollPercent = scrollTop / docHeight;
+            setScrollPercentage(scrollPercent > 1 ? 1 : scrollPercent < 0 ? 0 : scrollPercent);
+            
+            setNavbarScrolled(scrollTop > 50);
+            setScrollUpVisible(scrollTop > 500);
         };
 
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
+    const handleLinkClick = () => {
+        setMenuOpen(false);
+    };
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+    }, [menuOpen]);
+
     return (
         <>
+            {/* Scroll Gauge */}
+            <div className={styles.gaugeContainer}>
+                <div className={styles.gauge} style={{ width: `${scrollPercentage * 100}%` }}></div>
+            </div>
 
-      {/* Scroll Gauge */}
-      <div className={styles.gaugeContainer}>
-        <div className={styles.gauge} style={{ width: `${scrollPercentage * 100}%` }}></div>
-      </div>
+            {/* Navbar */}
+            <nav className={`${styles.navbar} ${navbarScrolled ? styles.scrolled : ''}`}>
+                <div className={styles.navContainer}>
+                    <div className={styles.logo}>Thras</div>
+                    
+                    {/* Menu Desktop */}
+                    <div className={styles.menuItems}>
+                        <Link href="/" className={styles.menuItem}>HOME</Link>
+                        <Link href="/about" className={styles.menuItem}>ABOUT US</Link>
+                        <Link href="/expertise" className={styles.menuItem}>EXPERTISE</Link>
+                        <Link href="/careers" className={styles.menuItem}>CAREERS</Link>
+                        <Link href="/news" className={styles.menuItem}>NEWS</Link>
+                        <Link href="/contact" className={styles.menuItem}>CONTACT</Link>
+                    </div>
 
-      {/* Navbar */}
-      <nav className={`${styles.navbar} ${navbarScrolled ? styles.scrolled : ''}`}>
-        <div className={styles.navContainer}>
-          <div className={styles.logo}>Thras</div>
-<div className={styles.menuItems}>
-  <Link href="/" className={styles.menuItem}>HOME</Link>
-  <Link href="/about" className={styles.menuItem}>ABOUT US</Link>
-  <Link href="/expertise" className={styles.menuItem}>EXPERTISE</Link>
-  <Link href="/careers" className={styles.menuItem}>CAREERS</Link>
-  <Link href="/news" className={styles.menuItem}>NEWS</Link>
-  <Link href="/contact" className={styles.menuItem}>CONTACT</Link>
-          </div>
-        </div>
-      </nav>
+                    {/* Burger Button */}
+                    <div 
+                        className={`${styles.burger} ${menuOpen ? styles.burgerOpen : ''}`}
+                        onClick={() => setMenuOpen(!menuOpen)}
+                    >
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </nav>
+
+            {/* Menu Mobile Overlay */}
+            <div className={`${styles.mobileMenu} ${menuOpen ? styles.mobileMenuOpen : ''}`}>
+                <div className={styles.mobileMenuContent}>
+                    <Link href="/" className={styles.mobileMenuItem} onClick={handleLinkClick}>HOME</Link>
+                    <Link href="/about" className={styles.mobileMenuItem} onClick={handleLinkClick}>ABOUT US</Link>
+                    <Link href="/expertise" className={styles.mobileMenuItem} onClick={handleLinkClick}>EXPERTISE</Link>
+                    <Link href="/careers" className={styles.mobileMenuItem} onClick={handleLinkClick}>CAREERS</Link>
+                    <Link href="/news" className={styles.mobileMenuItem} onClick={handleLinkClick}>NEWS</Link>
+                    <Link href="/contact" className={styles.mobileMenuItem} onClick={handleLinkClick}>CONTACT</Link>
+                </div>
+            </div>
         </>
     );
 }
